1d254d125537fc2961e417c5f07bdb02a116fc3d
liuhongyang <994668759@qq.com>
2021-02-02 20:35:00 +0800

    update

--- FilterRender/Filter/GPUImageGreenScreenFilter.m
+++ FilterRender/Filter/GPUImageGreenScreenFilter.m
@@ -39,49 +39,9 @@ NSString *const kGPUImageGreenScreenFragmentShaderString = SHADER_STRING
     }else{
         gl_FragColor = textureColor;
     }
-//     textureColor.a = textureColor.a * textureColor.a * textureColor.a;
-//     gl_FragColor = textureColor;
  }
 );
 
-/*
- // lookup the color of the texel corresponding to the fragment being
- // generated while rendering a triangle
- lowp vec4 tempColor = texture2D(uVideoframe, vCoordinate);
- 
- // Calculate the average intensity of the texel's red and blue components
- lowp float rbAverage = tempColor.r * 0.5 + tempColor.b * 0.5;
- 
- // Calculate the difference between the green element intensity and the
- // average of red and blue intensities
- lowp float gDelta = tempColor.g - rbAverage;
- 
- // If the green intensity is greater than the average of red and blue
- // intensities, calculate a transparency value in the range 0.0 to 1.0
- // based on how much more intense the green element is
- tempColor.a = 1.0 - smoothstep(0.0, 0.25, gDelta);
- 
- // Use the cube of the of the transparency value. That way, a fragment that
- // is partially translucent becomes even more translucent. This sharpens
- // the final result by avoiding almost but not quite opaque fragments that
- // tend to form halos at color boundaries.
- tempColor.a = tempColor.a * tempColor.a * tempColor.a;
-    
-  gl_FragColor = tempColor;
- 
- */
-
-
-/*
- 
- textureColor.r == 75.0 && textureColor.g == 249.0 && textureColor.b == 44.0
- 
- R:75
-
- G:249
-
- B:44
- */
 
 @implementation GPUImageGreenScreenFilter
 
